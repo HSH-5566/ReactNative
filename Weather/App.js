f2ba50e73d77ec037fa0f6002b547b4f6376e751
HSH-5566 <cat5566@kyonggi.ac.kr>
2021-12-03 23:59:40 +0900

    [Add]WeatherAppStyles

--- Weather/App.js
+++ Weather/App.js
@@ -1,12 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native';
+const {width: DEVICE_WIDTH} = Dimensions.get('window');
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hellooo!!!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Kyonggi</Text>
+      </View>
+      <ScrollView
+         horizontal pagingEnabled showsHorizontalScrollIndicator={false}
+         contentContainerStyle={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>20º</Text>
+          <Text style={styles.description}>Cloud</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>20º</Text>
+          <Text style={styles.description}>Cloud</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>20º</Text>
+          <Text style={styles.description}>Cloud</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>20º</Text>
+          <Text style={styles.description}>Cloud</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>20º</Text>
+          <Text style={styles.description}>Cloud</Text>
+        </View>
+      </ScrollView>
+      <StatusBar style="dark" />
     </View>
   );
 }
@@ -14,8 +41,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'orange',
+  },
+  city: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  cityName: {
+    fontSize: 60,
+    fontWeight: 'bold',
+  },
+  weather: {
+  },
+  day: {
+    flex: 1,
+    width: DEVICE_WIDTH,
+    alignItems: 'center',
+  },
+  temp: {
+    fontSize: 140,
+    fontWeight: '800',
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 40,
   },
 });
