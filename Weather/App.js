d15405d6356cf401c3fed4f3de0aa1df2852c3dd
HSH-5566 <cat5566@kyonggi.ac.kr>
2021-12-05 01:27:02 +0900

    [Add]WeatherApp_날씨&위치&온도API이용

--- Weather/App.js
+++ Weather/App.js
@@ -1,37 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { StyleSheet, Text, View, ScrollView, Dimensions, ActivityIndicator } from 'react-native';
+import * as Location from 'expo-location';
+
 const {width: DEVICE_WIDTH} = Dimensions.get('window');
+const API_KEY = 'API_KEY';
 
 export default function App() {
+
+  const [city, setCity] = useState('Loading...');
+  const [country, setCountry] = useState('Loading...');
+  const [days, setDays] = useState([]);
+  const [ok, setOk] = useState(true);
+
+  async function searchWeather() {
+    const {granted} = await Location.requestForegroundPermissionsAsync();
+    if(!granted){
+      setOk(false);
+      console.log("Error");
+    }
+    const {coords:{latitude, longitude}} = await Location.getCurrentPositionAsync({accuracy: 5});
+    const location = await Location.reverseGeocodeAsync({latitude, longitude},{useGoogleMaps:false});
+    setCity(location[0].city +" "+ location[0].district);
+    setCountry(location[0].country + " "+ location[0].region);
+
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`);
+    const json = await response.json();
+    setDays(json.daily);
+  }
+
+  useEffect(() => {
+    searchWeather();
+  }, [])
+
   return (
     <View style={styles.container}>
       <View style={styles.city}>
-        <Text style={styles.cityName}>Kyonggi</Text>
+        <Text style={styles.countryName}>{country}</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
       <ScrollView
          horizontal pagingEnabled showsHorizontalScrollIndicator={false}
          contentContainerStyle={styles.weather}>
-        <View style={styles.day}>
-          <Text style={styles.temp}>20º</Text>
-          <Text style={styles.description}>Cloud</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>20º</Text>
-          <Text style={styles.description}>Cloud</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>20º</Text>
-          <Text style={styles.description}>Cloud</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>20º</Text>
-          <Text style={styles.description}>Cloud</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>20º</Text>
-          <Text style={styles.description}>Cloud</Text>
-        </View>
+        {days.length === 0 ? 
+          (<View style={styles.day}>
+            <ActivityIndicator color="white"/>
+          </View>) : 
+          (days.map((day, idx) => (
+            <View key={idx} style={styles.day}>
+              <Text style={styles.temp}>{parseFloat(day.temp.day).toFixed(1)}</Text>
+              <Text style={styles.description}>{day.weather[0].main}</Text>
+            </View>
+          )))
+        }
       </ScrollView>
       <StatusBar style="dark" />
     </View>
@@ -50,6 +71,10 @@ const styles = StyleSheet.create({
   },
   cityName: {
     fontSize: 60,
+    fontWeight: 'bold',
+  },
+  countryName: {
+    fontSize: 30,
     fontWeight: 'bold',
   },
   weather: {
